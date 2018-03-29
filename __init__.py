bddb653b864b6cfef4699b70679448dfe19ed2b8
otapi <31888571+otapi@users.noreply.github.com>
2018-03-29 16:25:25 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,217 @@
+﻿#!/usr/bin/env python
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+from __future__ import (unicode_literals, division, absolute_import,
+						print_function)
+
+__license__   = 'GPL v3'
+__copyright__ = '2011-2018, Hoffer Csaba <csaba.hoffer@gmail.com>, Kloon <kloon@techgeek.co.in>, otapi <otapigems.com>'
+__docformat__ = 'restructuredtext hu'
+
+import time
+from Queue import Queue, Empty
+from lxml.html import fromstring
+from calibre import as_unicode
+from calibre.ebooks.metadata import check_isbn
+from calibre.ebooks.metadata.sources.base import Source, Option
+from calibre.utils.icu import lower
+from calibre.utils.cleantext import clean_ascii_chars
+import lxml, sys, traceback
+from calibre import browser
+import urllib
+from lxml.html import tostring
+
+class Antikvarium_hu(Source):
+	name					= 'Antikvarium_hu'
+	description				= _('Downloads metadata and cover from antikvarium.hu')
+	author					= 'Hoffer Csaba & Kloon & otapi'
+	version					= (2, 0, 0)
+	minimum_calibre_version = (0, 8, 0)
+
+	capabilities = frozenset(['identify', 'cover'])
+	touched_fields = frozenset(['title', 'authors', 'identifier:isbn', 'identifier:antik_hu', 'tags', 'comments', 'publisher', 'pubdate', 'series', 'language', 'languages'])
+	has_html_comments = False
+	supports_gzip_transfer_encoding = False
+
+
+	KEY_MAX_DOWNLOADS = 'maxDownloads'
+	
+	options = [Option(KEY_MAX_DOWNLOADS, 'number', 3, _('Maximum number of books to get'),
+                      _('The maximum number of books to process from the Antikvarium search result')),
+	
+	]
+	
+	BASE_URL = 'https://www.antikvarium.hu'
+	BOOK_URL = BASE_URL + '/konyv/'
+	
+	def create_query(self, log, title=None, authors=None, identifiers={}):
+		if title is not None:
+			search_title = urllib.quote(title.encode('utf-8'))
+		else:
+			search_title = ''
+		log.info(' Title: %s'%search_title)
+		
+		if authors is not None:
+			search_author = urllib.quote(authors[0].encode('utf-8'))
+		else:
+			search_author = ''
+		log.info(' Author: %s'%search_author)
+		
+		search_page = "https://www.antikvarium.hu/index.php?type=search&kc=%s&sz=%s&he=0&jk=0&reszletes=1&rend=kiadasevecsokk&oldaldb=60&kapelol=0&nezet=li&elist=egyebadat&interfaceid=102&oldalcount=1"%(search_title, search_author)
+		return search_page
+		
+	def get_cached_cover_url(self, identifiers):
+		url = None
+		antik_id = identifiers.get('antik_hu', None)
+		if antik_id is None:
+			isbn = identifiers.get('isbn', None)
+			if isbn is not None:
+				antik_id = self.cached_isbn_to_identifier(isbn)
+		if antik_id is not None:
+			url = self.cached_identifier_to_cover_url(antik_id)
+		return url
+	def cached_identifier_to_cover_url(self, id_):
+		with self.cache_lock:
+			url = self._get_cached_identifier_to_cover_url(id_)
+			if not url:
+				# Try for a "small" image in the cache
+				url = self._get_cached_identifier_to_cover_url('small/'+id_)
+			return url
+	def _get_cached_identifier_to_cover_url(self, id_):
+		# This must only be called once we have the cache lock
+		url = self._identifier_to_cover_url_cache.get(id_, None)
+		if not url:
+			# We could not get a url for this particular B&N id
+			# However we might have one for a different isbn for this book
+			# Barnes & Noble are not very consistent with their covers and
+			# it could be that the particular ISBN we chose does not have
+			# a large image but another ISBN we retrieved does.
+			key_prefix = id_.rpartition('/')[0]
+			for key in self._identifier_to_cover_url_cache.keys():
+				if key.startswith('key_prefix'):
+					return self._identifier_to_cover_url_cache[key]
+		return url
+	def identify(self, log, result_queue, abort, title, authors,
+			identifiers={}, timeout=30):
+		'''
+		Note this method will retry without identifiers automatically if no
+		match is found with identifiers.
+		'''
+		
+		matches = []
+		antik_id = identifiers.get('antik_hu', None)
+		isbn = check_isbn(identifiers.get('isbn', None))
+		br = browser()
+		log.info(u'\nTitle:%s\nAuthors:%s\n'%(title, authors))
+		if antik_id:
+			matches.append('%s%s'%(Antikvarium_hu.BOOK_URL, antik_id))
+		else:
+			if isbn:
+				matches.append('https://www.antikvarium.hu/index.php?type=search&isbn=%s'%(isbn))
+			else:
+				query = self.create_query(log, title=title, authors=authors, identifiers=identifiers)
+				if query is None:
+					log.error('Insufficient metadata to construct query')
+					return
+				try:
+					log.info('Querying: %s'%query)
+					response = br.open(query)
+				except Exception as e:
+					if isbn and callable(getattr(e, 'getcode', None)) and e.getcode() == 404:
+						# We did a lookup by ISBN but did not find a match
+						# We will fallback to doing a lookup by title author
+						log.info('Failed to find match for ISBN: %s'%isbn)
+					else:
+						err = 'Failed to make identify query: %r'%query
+						log.exception(err)
+						return as_unicode(e)
+						
+				try:
+					raw = response.read().strip()
+					raw = raw.decode('utf-8', errors='replace')
+					if not raw:
+						log.error('Failed to get raw result for query: %r'%query)
+						return
+					root = fromstring(clean_ascii_chars(raw))
+				except:
+					msg = 'Failed to parse Antikvarium.hu page for query: %r'%query
+					log.exception(msg)
+					return msg
+				self._parse_search_results(log, title, authors, root, matches, timeout)
+			
+		if abort.is_set():
+			
+			return
+		if not matches:
+			if identifiers and title and authors:
+				log.info('No matches found with identifiers, retrying using only'
+						' title and authors')
+				return self.identify(log, result_queue, abort, title=title,
+						authors=authors, timeout=timeout)
+			log.error('No matches found with query: %r'%query)
+			return
+		from calibre_plugins.antikvarium_hu.worker import Worker
+		workers = [Worker(url, result_queue, br, log, i, self) for i, url in
+				enumerate(matches)]
+		
+		for w in workers:
+			w.start()
+			# Don't send all requests at the same time
+			time.sleep(0.1)
+
+		while not abort.is_set():
+			a_worker_is_alive = False
+			for w in workers:
+				w.join(0.2)
+			
+				if abort.is_set():
+					break
+				if w.is_alive():
+					a_worker_is_alive = True
+			if not a_worker_is_alive:
+			
+				break
+		return None
+		
+	def _parse_search_results(self, log, title, authors, root, matches, timeout):
+		results = root.xpath('//*[@id="searchResultKonyvCim-listas"]/@href')
+		
+		max_results = self.prefs[Antikvarium_hu.KEY_MAX_DOWNLOADS]
+		for result in results:
+			book_url = 'https://www.antikvarium.hu/' + result
+			log.info('Book URL: %r'%book_url)		
+			matches.append(book_url)
+			if len(matches) >= max_results:
+				return
+	def download_cover(self, log, result_queue, abort, title=None, authors=None, identifiers={}, timeout=30):
+		cached_url = self.get_cached_cover_url(identifiers)
+		if cached_url is None:
+			log.info('No cached cover found, running identify')
+			rq = Queue()
+			self.identify(log, rq, abort, title=title, authors=authors, identifiers=identifiers)
+			if abort.is_set():
+				return
+			results = []
+			while True:
+				try:
+					results.append(rq.get_nowait())
+				except Empty:
+					break
+			results.sort(key=self.identify_results_keygen(
+				title=title, authors=authors, identifiers=identifiers))
+			for mi in results:
+				cached_url = self.get_cached_cover_url(mi.identifiers)
+				if cached_url is not None:
+					break
+		if cached_url is None:
+			log.info('No cover found')
+			return
+
+		if abort.is_set():
+			return
+		br = self.browser
+		log.info('Downloading cover from:', cached_url)
+		try:
+			cdata = br.open_novisit(cached_url, timeout=timeout).read()
+			result_queue.put((self, cdata))
+		except:
+			log.exception('Failed to download cover from:', cached_url)
---
+++ worker.py
@@ -0,0 +1,327 @@
+﻿#!/usr/bin/env python
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+from __future__ import (unicode_literals, division, absolute_import,
+						print_function)
+
+__license__   = 'GPL v3'
+__copyright__ = '2011-2012, Hoffer Csaba <csaba.hoffer@gmail.com>, Kloon <kloon@techgeek.co.in>'
+__docformat__ = 'restructuredtext hu'
+
+import socket, re
+from threading import Thread
+from calibre.ebooks.metadata.book.base import Metadata
+import lxml, sys
+import lxml.html as lh
+from calibre.utils.date import utcnow
+from datetime import datetime
+from dateutil import parser
+from calibre.ebooks.metadata import MetaInformation
+from calibre import browser
+
+
+class Worker(Thread): # Get details
+
+	'''
+	Get book details from antikvarium.hu book page in a separate thread
+	'''
+
+	def __init__(self, url, result_queue, browser, log, relevance, plugin, timeout=30):
+		Thread.__init__(self)
+		self.daemon = True
+		self.url, self.result_queue = url, result_queue
+		self.log, self.timeout = log, timeout
+		self.relevance, self.plugin = relevance, plugin
+		self.browser = browser.clone_browser()
+		self.cover_url = self.antik_id = self.isbn = None
+
+	def run(self):
+		try:
+			self.get_details()
+		except:
+			self.log.exception('get_details failed for url: %r'%self.url)
+
+	def get_details(self):
+		
+		try:
+			raw = self.browser.open_novisit(self.url, timeout=self.timeout)
+		except Exception as e:
+			if callable(getattr(e, 'getcode', None)) and e.getcode() == 404:
+				self.log.error('URL malformed: %r'%self.url)
+				return
+			attr = getattr(e, 'args', [None])
+			attr = attr if attr else [None]
+			if isinstance(attr[0], socket.timeout):
+				msg = 'Antikvarium.hu timed out. Try again later.'
+				self.log.error(msg)
+			else:
+				msg = 'Failed to make details query: %r'%self.url
+				self.log.exception(msg)
+			return
+
+		root = lh.parse(raw)
+		self.parse_details(root)
+		
+		
+
+	def parse_details(self, root):
+		try:
+			antik_id = self.parse_antik_id(root)
+			self.log.info('Parsed Antikvarium identifier: %s'%antik_id)
+		except:
+			self.log.exception('Error parsing Antikvarium id for url: %r'%self.url)
+			antik_id = None
+
+		try:
+			title = self.parse_title(root)
+			self.log.info('Parsed title: %s'%title)
+		except:
+			self.log.exception('Error parsing title for url: %r'%self.url)
+			title = None
+		
+		try:
+			authors = self.parse_authors(root)
+			self.log.info('Parsed authors: %s'%authors)
+		except:
+			self.log.exception('Error parsing authors for url: %r'%self.url)
+			authors = []
+
+		if not title or not authors or not antik_id:
+			self.log.error('Could not find title/authors/Antikvarium.hu id for %r'%self.url)
+			self.log.error('Antikvarium.hu id: %r Title: %r Authors: %r'%(antik_id, title, authors))
+			return
+
+		mi = Metadata(title, authors)
+		mi.set_identifier('antik_hu', antik_id)
+		self.antik_id = antik_id
+
+		try:
+			isbn = self.parse_isbn(root)
+			self.log.info('Parsed ISBN: %s'%isbn)
+			if isbn:
+				self.isbn = mi.isbn = isbn
+		except:
+			self.log.exception('Error parsing ISBN for url: %r'%self.url)
+
+		try:
+			series = self.parse_series(root)
+			self.log.info('Parsed series: %s'%series)
+		except :
+			self.log.exception('Error parsing series for url: %r'%self.url)
+			series = None
+			
+		try:
+			mi.series_index = self.parse_series_index(root)
+			self.log.info('Parsed series index: %s'%mi.series_index)
+		except :
+			self.log.exception('Error parsing series for url: %r'%self.url)
+			mi.series_index = None
+			
+		try:
+			mi.comments = self.parse_comments(root)
+			self.log.info('Parsed comments: %s'%mi.comments)
+		except:
+			self.log.exception('Error parsing comments for url: %r'%self.url)
+
+		try:
+			self.cover_url = self.parse_cover(root)
+			self.log.info('Parsed URL for cover: %r'%self.cover_url)
+			self.plugin.cache_identifier_to_cover_url(self.antik_id, self.cover_url)
+			mi.has_cover = bool(self.cover_url)
+		except:
+			self.log.exception('Error parsing cover for url: %r'%self.url)
+
+		try:
+			mi.publisher = self.parse_publisher(root)
+			self.log.info('Parsed publisher: %s'%mi.publisher)
+		except:
+			self.log.exception('Error parsing publisher for url: %r'%self.url)
+			
+		try:
+			mi.tags = self.parse_tags(root)
+			self.log.info('Parsed tags: %s'%mi.tags)
+		except:
+			self.log.exception('Error parsing tags for url: %r'%self.url)
+
+		try:
+			mi.pubdate = self.parse_published_date(root)
+			self.log.info('Parsed publication date: %s'%mi.pubdate)
+		except:
+			self.log.exception('Error parsing published date for url: %r'%self.url)
+			
+		try:
+			mi.languages = self.parse_languages(root)
+			self.log.info('Parsed languages: %r'%mi.languages)
+		except:
+			self.log.exception('Error parsing languages for url: %r'%self.url)
+
+		mi.source_relevance = self.relevance
+
+		if series:
+			mi.series = series
+
+		if self.antik_id and self.isbn:
+			self.plugin.cache_isbn_to_identifier(self.isbn, self.antik_id)
+
+		
+		self.plugin.clean_downloaded_metadata(mi)
+
+		self.result_queue.put(mi)
+
+	def parse_antik_id(self, root):
+		try:
+			antik_id_node = root.xpath('/html/head/link/@href')
+			for antik_id in antik_id_node:
+				m = re.search('/konyv/(.*)', antik_id)
+				if m:
+					return m.group(1)
+		except:
+			return None
+		
+	def book_property(self, root, search_data):
+		for i in range(1, 12):
+			try:
+				data = root.xpath('//*[@class="book-data-table"]//tr[%d]/th//text()'%i)
+				if data:
+					data_text = data[0].strip()
+					if data_text == search_data:
+						data_nodes = root.xpath('//*[@class="book-data-table"]//tr[%d]/td//text()'%i)
+						for data in data_nodes:
+							if data.strip(' \r\n\t'):
+								return data.strip(' \r\n\t')
+				i = i + 1
+			except:
+				return None
+		
+		'''try:
+			data = root.xpath('//*[@id="konyvadat_adatok"]//tr[%d]/td[2]//text()'%index)
+			if data:
+				return data.strip()
+				return [unicode(data_node.strip(' \r\n\t')) for data_node in data_nodes]
+		except:
+			return None'''
+		
+	def parse_title(self, root):
+		title_node = root.xpath('//*[@class="book-data-title-height"]//text()')
+		return title_node[0].strip(' \r\n\t')
+			
+	def parse_series(self, root):
+		try:
+			search_data = "Sorozatcím:"
+			isbn = self.book_property(root, search_data)
+			if isbn:
+				return isbn.replace('-', '')
+		except:
+			return None
+		
+		
+	def parse_series_index(self, root):
+		try:
+			search_data = "Kötetszám:"
+			isbn = self.book_property(root, search_data)
+			if isbn:
+				return isbn.replace('-', '')
+		except:
+			return None
+
+	def parse_authors(self, root):
+		try:
+			author_nodes = root.xpath('//*[@class="book-data-author"]//text()')
+			authors = []
+			for i in range(0, len(author_nodes), 1):
+				if author_nodes[i].strip(' \r\n\t'):
+					authors.append(author_nodes[i].strip(' \r\n\t'))
+			return authors
+		except:
+			return None
+
+	def parse_isbn(self, root):
+		try:
+			search_data = "ISBN:"
+			isbn = self.book_property(root, search_data)
+			if isbn:
+				return isbn.replace('-', '')
+		except:
+			return None
+	def parse_publisher(self, root):
+		try:
+			search_data = "Kiadó:"
+			publisher_node = self.book_property(root, search_data)
+			if publisher_node:
+				return publisher_node
+		except:
+			return None
+
+	def parse_published_date(self, root):
+		try:
+			search_data = "Kiadás éve:"
+			pub_year_node = self.book_property(root, search_data)
+			if pub_year_node:
+				default = datetime.utcnow()
+				from calibre.utils.date import utc_tz
+				default = datetime(default.year, default.month, default.day, tzinfo=utc_tz)
+				pub_date = parser.parse(pub_year_node[0], default=default)
+				return pub_date
+		except:
+			return None
+			
+	def parse_tags(self, root):
+		try:
+			tag_nodes = root.xpath('//*[@id="konyvAdatlapTemakorLink"]/span/text()')
+			tags = []
+			for i in range(0, len(tag_nodes), 1):
+				tag = tag_nodes[i]
+				if tag != 'Tartalom szerint' and tag != 'Egyéb' and tag != 'Az író származása szerint':
+					if tag not in tags:
+						tags.append(tag)
+			
+			return tags
+		except:
+			return None
+	
+	def parse_comments(self, root):
+		try:
+			comments = root.xpath('//*[@id="fulszovegShort"]//text()[2]')
+			if comments:
+				return comments[1].strip(' \r\n\t')
+			comments = root.xpath('//*[@id="eloszoFull"]//text()[2]')
+			if comments:
+				return comments[1].strip(' \r\n\t')
+				
+			
+		except:
+			return None
+	
+	def parse_languages(self, root):
+		try:
+			search_data = "Nyelv:"
+			lang_node = self.book_property(root, search_data)
+			
+			return [self._translateLanguageToCode(lang.lower()) for lang in lang_node.split(',')]
+		except:
+			return None
+
+	def parse_cover(self, root):
+		try:
+			book_cover = root.xpath('//*[@class="konyvadatlapfoto"]/img/@src')
+			if book_cover:
+				return book_cover[0]
+		except:
+			return None
+		
+	def _translateLanguageToCode(self, displayLang):
+		displayLang = displayLang.strip() if displayLang else None
+		langTbl = { None: 'und',
+					u'magyar': 'hu', 
+					u'angol': 'en', 
+					u'amerikai': 'en',
+					u'amerikai angol': 'en', 
+					u'n\xe9met': 'de', 
+					u'francia': 'fr',
+					u'olasz': 'it', 
+					u'spanyol': 'es',
+					u'orosz': 'ru',
+					u't\xf6r\xf6k': 'tr',
+					u'g\xf6r\xf6g': 'gr',
+					u'k\xednai': 'cn' }
+		return langTbl.get(displayLang, None)
+		
